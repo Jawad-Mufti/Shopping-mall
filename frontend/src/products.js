f0e58a7deeb52d7dafb105ec66d02a63d37c4a75
Mufti <gusmofmo@student.gu.se>
2021-12-14 20:12:06 +0100

    create product data file, update images file in realtion to products

---
+++ frontend/src/products.js
@@ -0,0 +1,49 @@
+
+const products = [
+ { 
+_id: '1',
+name: 'candle shaped roben hood character',
+image: '/images/candle1.jpg',
+description:' candles made of a the best eco-friendly wax (bee wax) shaped in attractive styles and colored using natural coloring substances.',
+brand: 'chandler seven',
+category: 'Candels',
+price: 150,
+countInStock: 2,
+rating:4, 
+numReviews:5,
+ },
+ { 
+_id: '2',
+name: 'candle shaped jocker',
+image: '/images/candle2.jpg',
+description:' candles made of a the best eco-friendly wax (bee wax) shaped in attractive styles and colored using natural coloring substances.',
+brand: 'chandler seven',
+category: 'Candels',
+price: 150,
+countInStock: 7,
+rating:4, 
+numReviews:5},
+ { 
+_id :'3',
+name: 'bag',
+image: '/images/bag1.jpg',
+description:' High quality north african leather bag, imported from moroco, and highly durable.',
+brand:'chandler seven',
+category: 'Leather bags',
+price: 450,
+countInStock: 2,
+rating:4, 
+numReviews:5,
+},
+
+]
+
+export default products
+
+
+
+
+
+
+
+
